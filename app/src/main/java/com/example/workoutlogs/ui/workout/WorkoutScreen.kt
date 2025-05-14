84b317f3d449c9537bce64ddfebfa2b6be5766de
KropSdnir <phanzh@gmail.com>
2025-05-14 21:21:59 +0300

    added default exercises

--- app/src/main/java/com/example/workoutlogs/ui/workout/WorkoutScreen.kt
+++ app/src/main/java/com/example/workoutlogs/ui/workout/WorkoutScreen.kt
@@ -73,7 +73,7 @@ fun WorkoutScreen(navController: NavController) {
                         )
                     }
                     Text(
-                        text = "Workouts",
+                        text = "Workout",
                         style = MaterialTheme.typography.titleMedium
                     )
                     IconButton(
